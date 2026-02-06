acdae228d398d2864f9e3e80dd2be515d6f7be7b
yolisobral <yolisobrallores@gmail.com>
2026-02-06 14:15:16 +0100

    feat(tablaAsignacion): :sparkles: add tablaAsignacion

---
+++ src/tablaAsignacion.py
@@ -0,0 +1,32 @@
+class TablaAsignacion():
+   
+   
+    def __init__ (self):
+        self.tabla = [
+            "T",
+            "R",
+            "W",
+            "A",
+            "G",
+            "M",
+            "Y",
+            "F",
+            "P",
+            "D",
+            "X",
+            "B",
+            "N",
+            "J",
+            "Z",
+            "S",
+            "Q",
+            "V",
+            "H",
+            "L",
+            "C",
+            "K",
+            "E"
+        ]
+
+    def getTabla(self):
+        return self.tabla

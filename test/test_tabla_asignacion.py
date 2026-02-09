7614106b715fc41875c51cf207ee36140de9658c
DalilaManu <dmoliveira1205@gmail.com>
2026-02-09 14:10:48 +0100

    test(test_tabla_asignacion): :white_check_mark: add isLetraPermitida

--- test/test_tabla_asignacion.py
+++ test/test_tabla_asignacion.py
@@ -45,3 +45,10 @@ def test_getLetra(tabla):
 
 def test_getModulo(tabla):
     assert tabla.getModulo() == 23
+
+def test_isLetraPermitida(tabla):
+    assert tabla.isLetraPermitida("T")
+    assert not tabla.isLetraPermitida("I")
+
+
+

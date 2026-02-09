b85f60c3c3faa6dd92712a44c1b34b1c5ead6269
yolisobral <yolisobrallores@gmail.com>
2026-02-09 12:41:24 +0100

    refactor: :construction_worker: Refactoring tablaAsignacion

--- src/tablaAsignacion.py
+++ src/tablaAsignacion.py
@@ -1,7 +1,7 @@
 class TablaAsignacion():
    
-   
-    def __init__ (self):
+   # Crea la tabla de asignación de letras para el DNI
+    def __init__ (self):                        
         self.tabla = [
             "T",
             "R",
@@ -28,5 +28,26 @@ class TablaAsignacion():
             "E"
         ]
 
-    def getTabla(self):
+    # Devuelve la tabla de asignación de letras para el DNI
+    def getTabla(self):                         
         return self.tabla
+    
+    # Devuelve la letra correspondiente a la posición dada, menos si esta fuera de rango, en cuyo caso devuelve un mensaje de error
+    def getLetra(self, posicion):               
+        try:
+            return self.tabla[posicion]
+        except IndexError:                      
+            return "Posición fuera de rango"
+
+    # Devuelve el número de elementos en la tabla, que es el módulo para calcular la letra del DNI    
+    def getModulo(self):                        
+        return len(self.getTabla())
+    
+    # Calcula la letra del DNI a partir del número de DNI utilizando el módulo y la tabla de asignación
+    def calcularLetra(self, numero_dni): 
+        posicion = numero_dni % self.getModulo()
+        return self.getLetra(posicion)
+    
+    # Devuelve una representación en cadena de la tabla de asignación de letras para el DNI
+    def __repr__(self): 
+        return ' '.join(self.getTabla())

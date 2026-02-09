f795ddd6e46d1bb5e79d7ee9c01c4a0efaeb1750
DalilaManu <dmoliveira1205@gmail.com>
2026-02-09 13:58:32 +0100

    test(test_tabla_asignacion): :white_check_mark: Refactoring test

--- test/test_tabla_asignacion.py
+++ test/test_tabla_asignacion.py
@@ -1,6 +1,4 @@
 from src.tablaAsignacion import TablaAsignacion
-from test.dni_correctos import CASOS_TEST_CORRECTOS
-from test.dni_incorrectos import CASOS_TEST_LETRA_PROHIBIDA
 import pytest
 
 
@@ -39,31 +37,7 @@ def test_getTabla(tabla):
         "E",
     ]
 
-
 def test_getLetra(tabla):
     assert tabla.getLetra(0) == "T"
     assert tabla.getLetra(22) == "E"
     assert tabla.getLetra(30) == "Posicion letra fuera de rango"
-
-
-def test_getModulo(tabla):
-    assert tabla.getModulo() == 23
-
-
-def test_isLetraPermitida(tabla):
-    assert tabla.isLetraPermitida("T")
-    assert not tabla.isLetraPermitida("I")
-
-
-@pytest.mark.parametrize("dni", CASOS_TEST_CORRECTOS)
-def test_calcularLetra_correcta(tabla, dni):
-    numero_dni = dni[:-1]
-    letra = dni[-1]
-    assert tabla.calcularLetra(numero_dni) == letra
-
-
-@pytest.mark.parametrize("dni", CASOS_TEST_LETRA_PROHIBIDA)
-def test_calcularLetra_incorrecta(tabla, dni):
-    numero_dni = dni[:-1]
-    letra = dni[-1]
-    assert tabla.calcularLetra(numero_dni) != letra

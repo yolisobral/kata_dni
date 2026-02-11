fc407b79e146f68a36e2825f9ce5185904eb3855
yolisobral <yolisobrallores@gmail.com>
2026-02-11 13:21:15 +0100

    test: :white_check_mark: add test_calcularLetra_incorrecta

--- test/test_tabla_asignacion.py
+++ test/test_tabla_asignacion.py
@@ -1,5 +1,6 @@
 from src.tablaAsignacion import TablaAsignacion
 from test.dni_correctos import CASOS_TEST_CORRECTOS
+from test.dni_incorrectos import CASOS_TEST_LETRA_PROHIBIDA
 import pytest
 
 
@@ -57,3 +58,9 @@ def test_calcularLetra_correcta(tabla, dni):
     numero_dni = dni[:-1]
     letra = dni[-1]
     assert tabla.calcularLetra(numero_dni) == letra
+
+@pytest.mark.parametrize("dni", CASOS_TEST_LETRA_PROHIBIDA)
+def test_calcularLetra_incorrecta(tabla, dni):
+    numero_dni = dni[:-1]
+    letra = dni[-1]
+    assert tabla.calcularLetra(numero_dni) != letra
